8ab050f7f0a9c4f3f34f7fb1f958eafcf87db411
Ruslan Kutorgin <teenxsky@gmail.com>
2025-11-12 00:33:19 +1000

    Update repository

--- signature-server/internal/repository/contracts.go
+++ signature-server/internal/repository/contracts.go
@@ -2,13 +2,11 @@ package repository
 
 import (
 	"context"
-	"time"
 
 	"github.com/r0manch1k/umbrella/signature-server/internal/entity"
 )
 
 type LicenseRepository interface {
 	Save(ctx context.Context, license entity.License) error
-	GetByUserAndFingerprint(ctx context.Context, userID, hwFingerprint string) (*entity.License, error)
-	DeleteExpired(ctx context.Context, now time.Time) error
+	GetByFingerprint(ctx context.Context, license, fingerprint string) (*entity.License, error)
 }
